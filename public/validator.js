8a0b37e25589c2e387dce3ec069396cc431de2ea
andreykorolevich <korolevich1994@mail.ru>
2020-09-24 00:49:27 +0300

    Async validation form

--- public/validator.js
+++ public/validator.js
@@ -1,13 +1,59 @@
-const {body} = require('express-validator/check');
+const { body } = require('express-validator/check');
+const User = require('../model/user-model');
+const bcrypt = require('bcryptjs');
 
-exports.registerValidators = [ 
-    body('email', 'Input correctly email').isEmail(),
-    body('name', 'The name must be at least 3 characters long').isLength({min: 3}),
-    body('password', 'The password must be at least 8 characters long').isLength({min: 8}).isAlphanumeric(),
-    body('repeat').custom((value, {req}) => {
-        if(value !== req.body.password) {
-            throw new Error('The passwords must be the same');
-        }
-        return true
-    })
-]
+exports.registerValidators = [
+    body('email', 'Input correctly email')
+        .isEmail()
+        .custom(async (value, { req }) => {
+            try {
+                const user = await User.findOne({ email: value });
+                if (user) {
+                    return Promise.reject('There`s already user with such email');
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        })
+        .normalizeEmail(),
+    body('name', 'The name must be at least 3 characters long')
+        .isLength({ min: 3 })
+        .trim(),
+    body('password', 'The password must be at least 8 characters long')
+        .isLength({ min: 8 })
+        .isAlphanumeric()
+        .trim(),
+    body('repeat')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('The passwords must be the same');
+            }
+            return true
+        })
+        .trim()
+];
+
+exports.loginValidators = [
+    body('email')
+        .custom(async (value) => {
+            try {
+                const user = await User.findOne({ email: value });
+                if (!user) {
+                    return Promise.reject('There`s no such email or wrong password');
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }),
+    body('password')
+        .custom(async (value, { req }) => {
+            try {
+                const user = await User.findOne({ email: req.body.email });
+                const pas = await bcrypt.compare(value, user.password);
+                if (!pas) {
+                    return Promise.reject('There`s no such email or wrong password');
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        })]
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
-const { validationResult} = require('express-validator/check')
+const { validationResult } = require('express-validator/check')
 const User = require('../model/user-model');
 const bcrypt = require('bcryptjs');
 const router = Router();
-const {registerValidators} = require('../public/validator')
+const { registerValidators, loginValidators } = require('../public/validator')
 
 
 router.get('/login', async (req, res) => {
@@ -21,25 +21,23 @@ router.get('/logout', (req, res) => {
     })
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', loginValidators, async (req, res) => {
     try {
         const { email, password } = req.body;
-        let user = await User.findOne({ email });
-        const pas = await bcrypt.compare(password, user.password);
-        if (pas) {
-            req.session.user = user
-            req.session.isAuthenticated = true;
-            req.session.save(err => {
-                if (err) {
-                    throw err;
-                }
-                res.redirect('/')
-            })
-        } else {
-            req.flash('logError', 'There`s no such email or wrong password');
-            res.redirect('/auth/login#login')
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            req.flash('logError', errors.array()[0].msg);
+            return res.status(422).redirect('/auth/login#login');
         }
-
+        const user = await User.findOne({ email });
+        req.session.user = user;
+        req.session.isAuthenticated = true;
+        req.session.save(err => {
+            if (err) {
+                throw err;
+            }
+            res.redirect('/')
+        })
     } catch (err) {
         console.log(err);
     }
@@ -48,25 +46,18 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', registerValidators, async (req, res) => {
     try {
-        const { email, name, password, repeat } = req.body;
-        let user = await User.findOne({ email });
-
-        const errors = validationResult(req); 
-        if(!errors.isEmpty()){
+        const { email, name, password } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
             req.flash('regError', errors.array()[0].msg);
             return res.status(422).redirect('/auth/login#register');
         }
-        if (user) {
-            req.flash('regError', 'There`s already user with such email');
-            res.redirect('/auth/login#register');
-        } else {
-            const hashPassword = await bcrypt.hash(password, 12);
-            user = new User({
-                email, name, password: hashPassword, shopCart: { elements: [] }
-            })
-            await user.save();
-            res.redirect('login#login');
-        }
+        const hashPassword = await bcrypt.hash(password, 12);
+        user = new User({
+            email, name, password: hashPassword, shopCart: { elements: [] }
+        })
+        await user.save();
+        res.redirect('login#login');
     } catch (err) {
         console.log(err);
     }
